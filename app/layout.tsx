38545a5f641fd731d88c2f0067e8c2c73aaaaa6b
Crryz <104697733+Crryz@users.noreply.github.com>
2023-04-30 10:00:55 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "IntentGPT",
+  description: "Your personal IntentGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "IntentGPT",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
